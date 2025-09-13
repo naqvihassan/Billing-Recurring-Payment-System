0e83169380908a52d929e802fbb060a2c5ad56a3
Hassan <apple@Hassans-MacBook-Pro.local>
2025-09-14 00:52:06 +0500

    Deployed DB on supabase

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,12 +3,12 @@ const path = require("path");
 const { Sequelize, DataTypes } = require("sequelize");
 const dbConfig = require("../config/db");
 
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  dbConfig
-);
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
+  port: dbConfig.port,
+  dialect: dbConfig.dialect,
+  dialectOptions: dbConfig.dialectOptions,
+});
 
 const db = {};
 
